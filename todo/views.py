eb625430f2d734c365df74748b973ff0ff781503
ksh0rt <kshort2584@gmail.com>
2020-11-20 22:39:49 -0800

    Error handling for mismatched passwords and non unique user names. Added new page for user to be redirected to when signing up

--- todo/views.py
+++ todo/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 
 def signupuser(request):
     if request.method == 'GET':
         return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-            user.save()
-        else:
-            print("hello")
-            #Handle error
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Username already taken'})
+            return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords do not match'})
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')

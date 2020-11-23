a83d7db85751ce4ee3069f3ec85edd6f30b2ef44
ksh0rt <kshort2584@gmail.com>
2020-11-23 11:21:03 -0800

    added login page and login functionality

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 
 def signupuser(request):
     if request.method == 'GET':
@@ -28,3 +28,14 @@ def logoutuser(request):
 
 def home(request):
     return render(request, 'todo/home.html')
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/loginuser.html', {'form':AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html', {'form':AuthenticationForm(), 'error': 'Incorrect username or password'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')

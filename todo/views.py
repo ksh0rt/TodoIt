b01c781e710abd66f434b4333f1fa252a271beba
ksh0rt <kshort2584@gmail.com>
2020-11-23 14:30:33 -0800

    added page, form, and functionality to create a todo item

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
+from .forms import TodoForm
 
 def signupuser(request):
     if request.method == 'GET':
@@ -39,3 +40,16 @@ def loginuser(request):
         else:
             login(request, user)
             return redirect('currenttodos')
+
+def createtodo(request):
+    if request.method == 'GET':
+        return render(request, 'todo/createtodo.html', {'form':TodoForm()})
+    else:
+        try:
+            form = TodoForm(request.POST)
+            newtodo = form.save(commit=False)
+            newtodo.user = request.user
+            newtodo.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/createtodo.html', {'form':TodoForm(), 'error':'Bad data passed in'})
